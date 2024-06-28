3a15df6937c964c8451159c9ea6fb12fa7c36ba4
Rodion Pavlovkii <rodionpavlovscky@yandex.ru>
2024-06-28 09:11:18 +0300

    Add debounce in Search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 
+import { debounce } from "lodash";
 import { SearchContext } from "../../pages/Home";
 import styles from "./Search.module.scss";
 import clearIcon from "../../assets/img/clear.svg";
 
 function Search() {
+  const [value, setValue] = React.useState("");
   const { searchValue, setSearchValue } = React.useContext(SearchContext);
 
   const onClickClear = () => {
     setSearchValue("");
+    setValue("");
     inputRef.current.focus();
+  };
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      console.log(str);
+      setSearchValue(str);
+    }, 500),
+    []
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
   };
 
   const inputRef = React.useRef();
@@ -18,8 +34,8 @@ function Search() {
     <div className={styles.root}>
       <input
         ref={inputRef}
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        value={value}
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Поиск пиццы ..."
       />
